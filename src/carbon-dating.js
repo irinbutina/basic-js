b5a3280f040b4efe6140e2d33adfb9c3789be887
irinbutina <1irinabutina@gmail.com>
2025-01-01 16:59:10 +0700

    feat: add functon countCats

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -19,7 +19,7 @@ const HALF_LIFE_PERIOD = 5730;
  */
 function dateSample(/* sampleActivity */) {
   throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+  // remove line with error and write your code here 
 }
 
 module.exports = {
